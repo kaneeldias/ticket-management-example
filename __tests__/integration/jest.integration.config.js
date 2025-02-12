c50c6d04580ed1dd5225dba1669faa5e0f410190
kaneeldias <kaneeldias@hotmail.com>
2025-02-12 18:03:13 +0530

    Refactor unit and integration tests

--- __tests__/integration/jest.integration.config.js
+++ __tests__/integration/jest.integration.config.js
@@ -2,8 +2,8 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     testMatch: ["**/__tests__/integration/**/*.test.ts"],
-    globalSetup: "<rootDir>/__tests__/integration/setup.ts",
-    globalTeardown: "<rootDir>/__tests__/integration/teardown.ts",
+    globalSetup: "<rootDir>/setup.ts",
+    globalTeardown: "<rootDir>/teardown.ts",
     transform: {
         "^.+\\.ts?$": "ts-jest",
     },
